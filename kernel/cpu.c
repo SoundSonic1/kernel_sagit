75765b15df504e3f77b57fe75f41f863530dd86e
Kien Dang Tran <kiendangtran0@gmail.com>
2018-04-30 18:01:20 +0200

    Silences WLAN, PCIe, and CPU suspend state kernel messages

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -644,7 +644,7 @@ int disable_nonboot_cpus(void)
 	 */
 	cpumask_clear(frozen_cpus);
 
-	pr_info("Disabling non-boot CPUs ...\n");
+	pr_debug("Disabling non-boot CPUs ...\n");
 	for_each_online_cpu(cpu) {
 		if (cpu == first_cpu)
 			continue;
